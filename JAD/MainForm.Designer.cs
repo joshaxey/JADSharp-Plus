7d3bcac3b7b964ff0c3a03b9909dd6e545df9bc2
Josh_Axey <josh.axey.3006@gmail.com>
2015-07-26 13:11:54 +1000

    First Official Commit
    Version 1, first official commit.

---
+++ JAD/MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace JAD
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDecompile = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtConsole = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.btnDecompile);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.txtFilename);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(874, 33);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(614, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(533, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDecompile
+            // 
+            this.btnDecompile.Location = new System.Drawing.Point(452, 4);
+            this.btnDecompile.Name = "btnDecompile";
+            this.btnDecompile.Size = new System.Drawing.Size(75, 23);
+            this.btnDecompile.TabIndex = 3;
+            this.btnDecompile.Text = "Decompile";
+            this.btnDecompile.UseVisualStyleBackColor = true;
+            this.btnDecompile.Click += new System.EventHandler(this.btnDecompile_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(371, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.AllowDrop = true;
+            this.txtFilename.Location = new System.Drawing.Point(106, 6);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(259, 20);
+            this.txtFilename.TabIndex = 1;
+            this.txtFilename.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFilename_DragDrop);
+            this.txtFilename.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFilename_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Java File (*.class):";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Java Class|*.class";
+            this.openFileDialog1.Title = "Open *.class file";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtConsole);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(874, 455);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsole.Location = new System.Drawing.Point(0, 0);
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.txtConsole.Size = new System.Drawing.Size(874, 455);
+            this.txtConsole.TabIndex = 0;
+            this.txtConsole.Text = "";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Java|*.java";
+            this.saveFileDialog1.Title = "Save as *.java file";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 488);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "JADSharp Plus";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDecompile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox txtConsole;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ JAD/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace JAD
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFilename.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Parse(string filename)
+        {
+            if (filename.Equals("")) return;
+
+            txtFilename.Text = filename;
+            
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("jadit.exe", "-p \"" + filename + "\"");
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            result = Regex.Replace(result, "// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.\r\n", "// Decompiled with JADSharp Plus by Josh_Axey\r\n");
+            result = Regex.Replace(result, "// Jad home page: http://www.kpdus.com/jad.html\r\n", "");
+            result = Regex.Replace(result, "// Decompiler options: packimports\\(3\\) \r\n", "");
+            string fileName = txtFilename.Text.Substring(txtFilename.Text.LastIndexOf("\\") + 1).Replace(".class", ".java");
+            result = Regex.Replace(result, "// Source File Name:   " + fileName + "\r\n\r\n", "// Source File Name: " + fileName + "\r\n\r\n");
+            txtConsole.Text = result;
+
+        }
+
+        private void btnDecompile_Click(object sender, EventArgs e)
+        {
+            Parse(txtFilename.Text);
+        }
+
+        private void txtFilename_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            foreach (string File in FileList)
+                Parse(File);
+        }
+
+        private void txtFilename_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtConsole.Text = "";
+            txtFilename.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string dir = "C:\\";
+            string fileName = "";
+            if (!txtFilename.Text.Equals(""))
+            {
+                dir = txtFilename.Text.Substring(0, txtFilename.Text.LastIndexOf("\\"));
+                fileName = txtFilename.Text.Substring(txtFilename.Text.LastIndexOf("\\") + 1).Replace(".class", ".java");
+            }
+            saveFileDialog1.InitialDirectory = dir;
+            saveFileDialog1.FileName = fileName;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, txtConsole.Text);
+            }
+        }
+    }
+}
